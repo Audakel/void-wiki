2aee5b0b7c4ff765863a8276cc447559e994be63
Greg Fitzgerald <greg@gregf.org>
2015-11-10 13:13:32 -0500

    Remove redundant home link

--- skins/VoidLinux/VoidLinuxMediaWiki.skin.php
+++ skins/VoidLinux/VoidLinuxMediaWiki.skin.php
@@ -106,9 +106,6 @@ class VoidLinuxMediaWikiTemplate extends QuickTemplate {
 
 					<div class="collapse navbar-collapse">
 						<ul class="nav navbar-nav">
-							<li>
-							<a href="<?php echo $this->data['nav_urls']['mainpage']['href'] ?>">Home</a>
-							</li>
 							<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Tools <span class="caret"></span></a>
 								<ul class="dropdown-menu">
